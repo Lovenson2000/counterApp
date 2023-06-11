4a989c2d98defa723a9454f69f09da456a9b7932
Lovenson2000 <beaucicotlovenson@gmail.com>
2023-06-11 20:49:53 +0800

    Using switch statement

--- index.js
+++ index.js
@@ -2,28 +2,40 @@
 let content = document.getElementById("content");
 let integerValue = parseInt(content.innerHTML);
 
-const arrayButton = document.getElementsByTagName("button");
+const arrayButton = document.getElementsByClassName("button-event");
 const buttons = Object.entries(arrayButton);
 buttons.forEach((button, index) => {
-    if (index === 0) { //Decrease by 1 for index 0
-        button = document.getElementById("btn-decrease").addEventListener("click", () => {
-            integerValue -= 1;
-            content.innerHTML = integerValue;
-        })
+    function changeValue(button, index) {
+        switch (index) {
+            case 0:
+                button = document.getElementById("btn-decrease").addEventListener("click", () => {
+                    integerValue -= 1;
+                    content.innerHTML = integerValue;
+                })
+                break;
+    
+            case 1:
+                button = document.getElementById("btn-reset").addEventListener("click", () => {
+                    integerValue = 0;
+                    content.innerHTML = integerValue;
+                })
+                break;
+    
+            case 2:
+                button = document.getElementById("btn-increase").addEventListener("click", () => {
+                    integerValue += 1;
+                    content.innerHTML = integerValue;
+                })
+            }
+        }
+
+    changeValue(button, index);
+    
     }
-    else if (index === 1) { //Reset for index 1
-        button = document.getElementById("btn-reset").addEventListener("click", () => {
-            integerValue = 0;
-            content.innerHTML = integerValue;
-        })
-    }
-    else if(index === 2) { //Increase by 1 for index 2
-        button = document.getElementById("btn-increase").addEventListener("click", () => {
-            integerValue += 1;
-            content.innerHTML = integerValue;
-        })      
-    }
-})
+)
+
+
+
 
 
 
