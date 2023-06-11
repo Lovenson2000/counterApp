eb8cc1d40f3f6b172ffbb86139139ce50493a5c6
Lovenson2000 <beaucicotlovenson@gmail.com>
2023-06-11 21:53:10 +0800

    More changes

--- index.js
+++ index.js
@@ -2,37 +2,32 @@
 let content = document.getElementById("content");
 let integerValue = parseInt(content.innerHTML);
 
-const arrayButton = document.getElementsByClassName("button-event");
-const buttons = Object.entries(arrayButton);
-buttons.forEach((button, index) => {
-    function changeValue(button, index) {
-        switch (index) {
-            case 0:
-                button = document.getElementById("btn-decrease").addEventListener("click", () => {
-                    integerValue -= 1;
-                    content.innerHTML = integerValue;
-                })
-                break;
-    
-            case 1:
-                button = document.getElementById("btn-reset").addEventListener("click", () => {
-                    integerValue = 0;
-                    content.innerHTML = integerValue;
-                })
-                break;
-    
-            case 2:
-                button = document.getElementById("btn-increase").addEventListener("click", () => {
-                    integerValue += 1;
-                    content.innerHTML = integerValue;
-                })
-            }
-        }
-
-    changeValue(button, index);
-    
+const arrayButton = document.querySelectorAll(".button-event");
+arrayButton.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        changeValue(index)
+        })
     }
 )
+
+function changeValue(index) {
+    switch (index) {
+        case 0:
+            integerValue -= 1;
+            break;
+
+        case 1:
+            integerValue = 0;
+            break;
+
+        case 2:
+            integerValue += 1;
+            
+    }
+    content.innerHTML = integerValue;
+}
+
+
 
 
 
