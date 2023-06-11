91f8b18184a35aa7620e03130e2df963206cdb0a
Lovenson2000 <beaucicotlovenson@gmail.com>
2023-06-11 19:07:30 +0800

    another change

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 
-let content = document.getElementById("content").innerHTML;
-let integerValue = parseInt(content);
+let content = document.getElementById("content");
+let integerValue = parseInt(content.innerHTML);
 
 const arrayButton = document.getElementsByTagName("button");
 const buttons = Object.entries(arrayButton);
 buttons.forEach((button, index) => {
-
-    if (index === 0) {    //Decrease by -1 for index 0
+    if (index === 0) { //Decrease by 1 for index 0
         button = document.getElementById("btn-decrease").addEventListener("click", () => {
             integerValue -= 1;
             content.innerHTML = integerValue;
         })
     }
-    else if (index === 1) {     // Reset to 0 for index 1
+    else if (index === 1) { //Reset for index 1
         button = document.getElementById("btn-reset").addEventListener("click", () => {
             integerValue = 0;
             content.innerHTML = integerValue;
         })
     }
-    else if(index === 2) {     //Increase by 1 for index 2
+    else if(index === 2) { //Increase by 1 for index 2
         button = document.getElementById("btn-increase").addEventListener("click", () => {
             integerValue += 1;
             content.innerHTML = integerValue;
